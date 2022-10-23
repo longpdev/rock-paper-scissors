8c63f819670ea6ac348229b6fef1418cf07d1423
Long Pham <longpham@Longs-Air.hitronhub.home>
2022-10-23 00:20:19 -0600

    recording scores and log if call displayResult()

--- scripts.js
+++ scripts.js
@@ -1,33 +1,61 @@
-function getComputerChoice(){
-    let num = Math.floor(Math.random()*3 + 1);
+let computerScore = 0;
+let userScore = 0;
+
+function getComputerChoice() {
+    let num = Math.floor(Math.random() * 3 + 1);
     if (num == 1) return "rock";
     if (num == 2) return "paper";
     return "scissors";
 }
 
 
-function playRound(playerSelection, computerSelection = getComputerChoice()){
+function playRound(playerSelection, computerSelection = getComputerChoice()) {
     playerSelection = playerSelection.toLowerCase();
-   if (playerSelection == computerSelection)
-    return "You tied" + ". You and the computer both chose " + playerSelection + ".";
-   if (playerSelection == "rock" && computerSelection == "paper")
-    return "You Lose! Paper beats rock.";
-    if (playerSelection == "rock" && computerSelection == "scissors")
-     return "You Win! Rock beats scissors.";
-    if (playerSelection == "paper" && computerSelection == "rock")
-     return "You Win! Paper beats Rock";
-    if (playerSelection == "paper" && computerSelection == "scissors")
-     return "You Lost! Scissors beat Paper.";
-    if (playerSelection == "scissors" && computerSelection == "rock")
-     return "You Lost! Rock beats scissors.";
-    if (playerSelection == "scissors" && computerSelection == "paper")
-     return "You Win! Scissors beat paper.";
+    if (playerSelection == computerSelection) {
+        return "You tied" + ". You and the computer both chose " + playerSelection + ".";
+    }
+    if (playerSelection == "rock" && computerSelection == "paper") {
+        computerScore++;
+        return "You Lose! Paper beats rock.";
+    }
+    if (playerSelection == "rock" && computerSelection == "scissors") {
+        userScore++
+        return "You Win! Rock beats scissors.";
+    }
+    if (playerSelection == "paper" && computerSelection == "rock") {
+        userScore++;
+        return "You Win! Paper beats Rock";
+    }
+    if (playerSelection == "paper" && computerSelection == "scissors") {
+        computerScore++;
+        return "You Lost! Scissors beat Paper.";
+    }
+    if (playerSelection == "scissors" && computerSelection == "rock") {
+        computerScore++;
+        return "You Lost! Rock beats scissors.";
+    }
+
+    if (playerSelection == "scissors" && computerSelection == "paper") {
+        userScore++
+        return "You Win! Scissors beat paper.";
+    }
     return "Something wrong with your input!";
 }
 
-function game(){
-    for (i =0; i < 5; i++){
+function game() {
+    for (i = 0; i < 5; i++) {
         playerSelection = prompt("What is your input?");
         console.log(playRound(playerSelection, getComputerChoice()));
     }
+}
+
+function displayResult(){
+    if (userScore > computerScore){
+        console.log("You win in total!!! Your score is " + userScore + ", while the computer's score is " + computerScore + ".");
+    }
+    else if (userScore < computerScore){
+        console.log("You lost in total :(. Your score is " + userScore + ", while the computer's score is " + computerScore + ".");
+    }
+    else
+        console.log("You tied with the computer! You both scored " + userScore + " wins." );
 }
